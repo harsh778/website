7e67533d830b36e0054573da7c40eabf077f9ab1
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2021-01-14 19:30:32 +0530

    implement node on gatsby server

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,3 +10,33 @@ require("prismjs/themes/prism-tomorrow.css")
 require("hooker/hooker.js")
 require("gatsby/gatsby.js")
 require("netlify-toml/netlify.ts")
+const { createFilePath } = require(gatsby-source-filesystem)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+      readme: md
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(JSON.stringify(result, null, 4))
+}
